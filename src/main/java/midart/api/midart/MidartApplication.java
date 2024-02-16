5ab1763de099bc5a965c06e7e9f0e82cf06dbeff
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2024-02-15 22:28:55 -0300

    init project

---
+++ src/main/java/midart/api/midart/MidartApplication.java
@@ -0,0 +1,13 @@
+package midart.api.midart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/midart/api/midart/MidartApplicationTests.java
@@ -0,0 +1,13 @@
+package midart.api.midart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
